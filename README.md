1e80a363e1026c0f869851ebc5014c7df2d472c3
kirsty.hames <kirsty.hames@kineo.com>
2020-03-17 16:39:13 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# adapt-footer
+
+**Footer** can be applied at menu and page level.
+
+## Settings overview
+
+**Footer** attributes include a textarea for adding content and a class field for providing custom theme styles.
+
+Some layout classes are pre-bundled:
+**reverse-content-direction** : Content is positioned at the end of the container. By default, content is positioned at the beginning of the container.
+**center-content** : Content is positioned at the center of the container.
+**space-between-content** : Content is positioned with space between items.
+**space-around-content** : Content is positioned with space before, between and after items.
+
+Layout classes are based on css display flex properties.
---
+++ js/adapt-footer.js
@@ -0,0 +1,42 @@
+define(function(require) {
+
+	var Adapt = require('coreJS/adapt');
+	var Backbone = require('backbone');
+
+	var footerView = Backbone.View.extend({
+
+		className: 'footer',
+
+		initialize: function() {
+		var footer = this.model.get('_footer');
+
+		if (!footer || !footer._isEnabled) return;
+			this.render();
+		},
+
+		render: function() {
+			var data = this.model.toJSON();
+			var template = Handlebars.templates['footer'];
+
+			this.$el.html(template(data))
+			_.defer(_.bind(this.postRender, this));
+		},
+
+		postRender: function() {
+			this.listenTo(Adapt, 'remove', this.remove);
+			this.$el.addClass(this.model.get('_footer')._classes);
+		}
+
+	});
+
+	Adapt.on('app:dataReady', function() {
+		if (!Adapt.course.get("_footer")) return;
+
+		Adapt.on('menuView:ready pageView:ready', function(view) {
+				new footerView({
+					model: view.model
+				}).$el.appendTo(view.$el);
+		});
+	});
+
+});
