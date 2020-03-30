09d34517274d495e4f7aca67a3060c83073c956f
Guy Willis <guy.willis@kineo.com>
2020-03-30 15:30:19 +0100

    BEM and SMACSS refactor

--- js/adapt-footer.js
+++ js/adapt-footer.js
@@ -1,36 +1,42 @@
-define(function(require) {
-
-  var Adapt = require('coreJS/adapt');
-  var Backbone = require('backbone');
+define([
+  'core/js/adapt'
+], function(Adapt) {
 
   var footerView = Backbone.View.extend({
 
     className: 'footer',
 
     initialize: function() {
-    var footer = this.model.get('_footer');
+      var footer = this.model.get('_footer');
+      if (!footer || !footer._isEnabled) return;
 
-    if (!footer || !footer._isEnabled) return;
+      // if _footerContent not specified for this page, assume we just want to copy what's in course.json
+      if (!footer._footerContent) {
+        footer._footerContent = Adapt.course.get('_footer')._footerContent;
+      }
+
       this.render();
     },
 
     render: function() {
+      var template = Handlebars.templates.footer;
       var data = this.model.toJSON();
-      var template = Handlebars.templates['footer'];
 
-      this.$el.html(template(data))
-      _.defer(_.bind(this.postRender, this));
+      this.$el.html(template(data));
+
+      _.defer(this.postRender.bind(this));
     },
 
     postRender: function() {
       this.listenTo(Adapt, 'remove', this.remove);
+
       this.$el.addClass(this.model.get('_footer')._classes);
     }
 
   });
 
-  Adapt.on('app:dataReady', function() {
-    if (!Adapt.course.get("_footer")) return;
+  Adapt.once('app:dataReady', function() {
+    if (!Adapt.course.get('_footer')) return;
 
     Adapt.on('menuView:ready pageView:ready', function(view) {
       new footerView({
