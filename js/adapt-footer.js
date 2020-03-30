eeba75ea0e2ddc64ec1bf912be14d663173913cd
Guy Willis <guy.willis@kineo.com>
2020-03-30 14:12:32 +0100

    Converted tabs to spaces, indentation amends

--- js/adapt-footer.js
+++ js/adapt-footer.js
@@ -1,42 +1,42 @@
 define(function(require) {
 
-	var Adapt = require('coreJS/adapt');
-	var Backbone = require('backbone');
+  var Adapt = require('coreJS/adapt');
+  var Backbone = require('backbone');
 
-	var footerView = Backbone.View.extend({
+  var footerView = Backbone.View.extend({
 
-		className: 'footer',
+    className: 'footer',
 
-		initialize: function() {
-		var footer = this.model.get('_footer');
+    initialize: function() {
+    var footer = this.model.get('_footer');
 
-		if (!footer || !footer._isEnabled) return;
-			this.render();
-		},
+    if (!footer || !footer._isEnabled) return;
+      this.render();
+    },
 
-		render: function() {
-			var data = this.model.toJSON();
-			var template = Handlebars.templates['footer'];
+    render: function() {
+      var data = this.model.toJSON();
+      var template = Handlebars.templates['footer'];
 
-			this.$el.html(template(data))
-			_.defer(_.bind(this.postRender, this));
-		},
+      this.$el.html(template(data))
+      _.defer(_.bind(this.postRender, this));
+    },
 
-		postRender: function() {
-			this.listenTo(Adapt, 'remove', this.remove);
-			this.$el.addClass(this.model.get('_footer')._classes);
-		}
+    postRender: function() {
+      this.listenTo(Adapt, 'remove', this.remove);
+      this.$el.addClass(this.model.get('_footer')._classes);
+    }
 
-	});
+  });
 
-	Adapt.on('app:dataReady', function() {
-		if (!Adapt.course.get("_footer")) return;
+  Adapt.on('app:dataReady', function() {
+    if (!Adapt.course.get("_footer")) return;
 
-		Adapt.on('menuView:ready pageView:ready', function(view) {
-				new footerView({
-					model: view.model
-				}).$el.appendTo(view.$el);
-		});
-	});
+    Adapt.on('menuView:ready pageView:ready', function(view) {
+      new footerView({
+        model: view.model
+      }).$el.appendTo(view.$el);
+    });
+  });
 
 });
